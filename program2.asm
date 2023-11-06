4adf8cb0928d4d20ba2ee44e478b4152cb6d5a44
jchen1411 <149224092+jchen1411@users.noreply.github.com>
2023-11-05 18:44:44 -0800

    Update program2.asm

--- program2.asm
+++ program2.asm
@@ -1,1 +1,26 @@
+main:
 
+li $v0, 5
+syscall
+move $a0, $v0
+
+li $t0, 0
+li $t1, 10
+
+loop:
+beq $t0, $t1, exit
+
+slti $t2, $a0, 90
+bne $t2, 1, exit
+
+slti $t3, $a0, 80
+bne $t3, 1, exit
+
+slti $t4, $a0, 70
+bne $t4, 1, exit
+
+slti $t5, $a0, 60
+bne $t5, 1, exit
+
+slti $t6, $a0, 0
+bne $t6, 1, exit
