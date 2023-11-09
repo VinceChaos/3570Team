96791590787b82f2320b5895e69f8a45ab02aab9
Cathy Jeeyun Ko <72629467+PinkBloss@users.noreply.github.com>
2023-11-09 00:01:46 -0800

    Update program2.asm
    Updated code for task 1.

--- program2.asm
+++ program2.asm
@@ -1,6 +1,11 @@
-#Task 1: Return a Letter Grade from User Input
+#CS 2640. 04
+#November 8, 2023
+#Group: Vincent Perez, Cathy Ko, Guanlin Chen
+#Program 2: Practice with Conditionals and Loops
 
-#Write an Assembly program that will return a letter grade given a Decimal int from the user. Include the following:
+#Task 1: Return letter grade from user input
+
+#Write an Assembly program that will return a letter grade given a Decimal int from the user. 
 	#a user menu
 	#a main label, a looping label, and an exit label
 	#a way for the user to continue getting letter grades or choose to exit
@@ -8,11 +13,13 @@
 	#re-prompt the user until a correct input is entered)
 
 .data
-    # Define menu and other string data here
-    mainMenu: .asciiz "\n\n\nPlease choose an option: \n 1: Get Letter Grade \n 2: Exit Program \n\n Enter '1' or '2' for your selection: "
-    enterDecimalScore: .asciiz "\n\n Enter a score as an integer value: "
-    exitMenu: .asciiz "\n\n Exiting..."
-    invalidScore: .asciiz "\n\n Invalid input! Please enter a decimal score between 0 and 100. \n\n"
+    menuSelection: .asciiz "\n ------------ MAIN MENU --------------\n \n (1) Get Letter Grade \n (2) Exit Program \n\n Enter '1' or '2' for your selection: "
+    enterDecimalScore: .asciiz "\n\n Please enter a score as an integer value: "
+    enterNewScore: .asciiz "\n\n Would you like to enter a new score? \n (Y)Yes   (N)No \n\n Enter 'Y' or 'N' for your selection: "
+    exitMenu: .asciiz "\n\n The program will now exit. "
+    invalidScore: .asciiz "\n\n Invalid Score! Please enter a decimal score between 0 and 100. \n\n"
+    invalidInput: .asciiz "\n\n Invalid input. Please enter either 'Y' for Yes or 'N' for No. \n\n"
+    extraScore: .asciiz "\n Extra credit! \n\n"
     A_grade: .asciiz "\n\n Letter Grade: A"
     B_grade: .asciiz "\n\n Letter Grade: B"
     C_grade: .asciiz "\n\n Letter Grade: C"
@@ -22,11 +29,12 @@
 .globl main
 
 main:
-    li $v0, 4 # syscall for print string
-    la $a0, mainMenu
+	
+    li $v0, 4 # syscall to print string
+    la $a0, menuSelection
     syscall
 
-    li $v0, 5 # syscall for read int
+    li $v0, 5 # syscall to print string
     syscall
 
     move $t0, $v0 # move the input into $t0
@@ -37,7 +45,7 @@ main:
     j main_loop # otherwise, jump back to main_loop
 
 getLetterGrade:
-    li $v0, 4 # syscall for print string
+    li $v0, 4 # syscall to print string
     la $a0, enterDecimalScore
     syscall
 
@@ -46,53 +54,95 @@ getLetterGrade:
 
     move $t1, $v0 # move the input into $t1
 
-    blt $t1, 0, invalid_score # if the score is less than 0, it's invalid
-        bgt $t1, 100, invalid_score # if the score is greater than 100, it's invalid
+    blt $t1, 0, invalid_score # if the score is less than 0, invalid
+    bgt $t1, 100, extra_credit # if the score is greater than 100, extra credit
 
     bge $t1, 90, print_A_grade # if the score is greater than or equal to 90, it's an A
     bge $t1, 80, print_B_grade # if the score is greater than or equal to 80, it's a B
     bge $t1, 70, print_C_grade # if the score is greater than or equal to 70, it's a C
     bge $t1, 60, print_D_grade # if the score is greater than or equal to 60, it's a D
 
-    # If the score is less than 60, it's an F
+    # If the score is less than 60, F grade
     print_F_grade:
-        li $v0, 4 # syscall for print string
+        li $v0, 4 # syscall to print string
         la $a0, F_grade
         syscall
+        j new_score
         j main_loop # jump back to main_loop
 
     print_D_grade:
-        li $v0, 4 # syscall for print string
+        li $v0, 4 # syscall to print string
         la $a0, D_grade
         syscall
+        j new_score
         j main_loop # jump back to main_loop
 
     print_C_grade:
-        li $v0, 4 # syscall for print string
+        li $v0, 4 # syscall to print string
         la $a0, C_grade
         syscall
+        j new_score
         j main_loop # jump back to main_loop
 
     print_B_grade:
-        li $v0, 4 # syscall for print string
+        li $v0, 4 # syscall to print string
         la $a0, B_grade
         syscall
+        j new_score
         j main_loop # jump back to main_loop
 
     print_A_grade:
-        li $v0, 4 # syscall for print string
+        li $v0, 4 # syscall to print string
         la $a0, A_grade
         syscall
+        j new_score
         j main_loop # jump back to main_loop
+        
+new_score:
+    #prompt the user if they want to continue with a new score
+    li $v0, 4
+    la $a0, enterNewScore #print the prompt
+    syscall
+    
+    li $v0, 12 #syscall to read character
+    syscall
+    
+    move $t1, $v0
+    
+    beq $t1, 'Y', getLetterGrade #if user wants to test a new score, loop through getLetterGrade
+    beq $t1, 'N', exit           #if user wants to quit, exit the program
+    
+    #Check for invalid input of "Y' or 'N'
+    bne $t1, 'Y', invalid_input
+    bne $t1, 'N', invalid_input
+
+    syscall
+    j new_score
+ 
 
 invalid_score:
-    li $v0, 4 # syscall for print string
+    li $v0, 4 # syscall to print string
     la $a0, invalidScore
     syscall
+    j new_score
     j main_loop # jump back to main_loop
+    
+invalid_input:
+    li $v0, 4 # syscall to print string
+    la $a0, invalidInput
+    syscall
+    j new_score
+    j main_loop
+    
+extra_credit:
+    li $v0, 4 # syscall to print string
+    la $a0, extraScore 
+    syscall
+    j new_score
+    j main_loop
 
 exit:
-    li $v0, 4 # syscall for print string
+    li $v0, 4 # syscall to print string
     la $a0, exitMenu
     syscall
 
@@ -101,6 +151,8 @@ exit:
 
 main_loop:
     j main # jump back to main
+   
+
    
 
 
